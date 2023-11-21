11847db55c7c8fdcc46874dae843eac14b04c9a9
chrisbyrne2696 <80651115+cd-byrne@users.noreply.github.com>
2023-11-21 10:44:21 -0600

    Update vite.config.js

--- portfolio-pages/vite.config.js
+++ portfolio-pages/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const basePath = '/portfolio-pages/'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
-  base: '/portfolio-pages/dist/',
-})
+  base: command === 'serve' ? '/' : './',
+}));
