934cc936e6dc5e0ac833b5fdc639a16e6ef2377b
chrisbyrne2696 <80651115+cd-byrne@users.noreply.github.com>
2023-11-20 21:23:32 -0600

    dynamic env

--- portfolio-pages/vite.config.js
+++ portfolio-pages/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const productionBase = '/portfolio-pages/';
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
-  base: './',
-})
+  base: command === 'build' ? productionBase : '/',
+}));
