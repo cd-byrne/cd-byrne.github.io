0772608e9fd2fc63ac009c2be7232ff867bd7857
chrisbyrne2696 <80651115+cd-byrne@users.noreply.github.com>
2023-11-21 10:47:10 -0600

    cleanup

--- portfolio-pages/vite.config.js
+++ portfolio-pages/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const basePath = '/portfolio-pages/'
-
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [vue()],
