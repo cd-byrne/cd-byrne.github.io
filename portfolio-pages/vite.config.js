63c27b1eb522ed3be2d69ba1fe116fc8dca3c6e3
chrisbyrne2696 <80651115+cd-byrne@users.noreply.github.com>
2023-11-20 21:11:49 -0600

    relative

--- portfolio-pages/vite.config.js
+++ portfolio-pages/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/cd-byrne.github.io/portfolio-pages/dist/',
+  base: './',
 })
